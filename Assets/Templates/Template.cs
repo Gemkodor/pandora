0eb534a3bc98bd7edd7d8e707f3943c5d2aa03ad
Maxime Arthaud <maxime@arthaud.me>
2021-10-16 15:48:27 -0700

    Add a template with asserts and new input system examples

---
+++ Assets/Templates/Template.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+public class Template : MonoBehaviour {
+    [SerializeField] InputAction _mouseClick;
+
+    void Start() {
+        // Example of getting a component.
+        var rigidBody = GetComponent<Rigidbody>();
+        Assert.IsNotNull(rigidBody);
+    }
+
+    void OnEnable() {
+        _mouseClick.Enable();
+    }
+
+    void OnDisable() {
+        _mouseClick.Disable();
+    }
+
+    void Update() {
+        // Example of new input system.
+        if (_mouseClick.triggered) {
+            Debug.Log("Test");
+        }
+    }
+}
