b6464013892ee950fc91b88a99bb37643e1caba3
Maxime Arthaud <maxime@arthaud.me>
2021-10-26 21:13:09 -0700

    Add a simple animation on the loading screen

---
+++ Assets/Scenes/LoadingScreen/LoadingTextAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class LoadingTextAnimation : MonoBehaviour {
+    [SerializeField] string _message = "Loading";
+    [SerializeField] float _delay = 0.5f;
+
+    void Start() {
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText() {
+        var text = GetComponent<TextMeshProUGUI>();
+        Assert.IsNotNull(text);
+
+        while (true) {
+            text.text = _message;
+            yield return new WaitForSeconds(_delay);
+            text.text = $"{_message}.";
+            yield return new WaitForSeconds(_delay);
+            text.text = $"{_message}..";
+            yield return new WaitForSeconds(_delay);
+            text.text = $"{_message}...";
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
