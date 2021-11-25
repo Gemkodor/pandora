0d1d732989c4599a608d3d0f8dabe25eda248660
Maxime Arthaud <maxime@arthaud.me>
2021-11-24 16:36:40 -0800

    Add a draw animation when aiming with right click

---
+++ Assets/Prefabs/Player/PlayerWeaponInput.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+namespace Pandora {
+
+public class PlayerWeaponInput : MonoBehaviour {
+    [Header("Controller Input")]
+    [SerializeField] InputAction _aimInput;
+    [SerializeField] InputAction _fireInput;
+
+    private Animator _animator;
+
+    public static class AnimatorParameters {
+        public static int IsAiming = Animator.StringToHash("IsAiming");
+    }
+
+    void Start() {
+        _animator = GetComponent<Animator>();
+        Assert.IsNotNull(_animator);
+    }
+
+    private void OnEnable() {
+        _aimInput.Enable();
+        _fireInput.Enable();
+    }
+
+    private void OnDisable() {
+        _aimInput.Disable();
+        _fireInput.Disable();
+    }
+
+    void Update() {
+        AimingInput();
+        FireInput();
+    }
+
+    void AimingInput() {
+        bool aiming = _aimInput.ReadValue<float>() > 0.5f;
+        _animator.SetBool(AnimatorParameters.IsAiming, aiming);
+    }
+
+    void FireInput() {
+        if (_fireInput.triggered) {
+            Debug.Log("BOUM!"); // TODO
+        }
+    }
+}
+
+}
