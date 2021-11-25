b5e93aa5a056827a936db1c539963d1c672ca4f6
Maxime Arthaud <maxime@arthaud.me>
2021-11-24 19:44:27 -0800

    Implement a basic shooting mechanic

--- Assets/Prefabs/Player/PlayerWeaponInput.cs
+++ Assets/Prefabs/Player/PlayerWeaponInput.cs
@@ -11,6 +11,12 @@ public class PlayerWeaponInput : MonoBehaviour {
     [SerializeField] InputAction _aimInput;
     [SerializeField] InputAction _fireInput;
 
+    [Header("Shooting Mechanic")]
+    [SerializeField] GameObject _arrowPrefab;
+    [SerializeField] Transform _arrowSpawn;
+    [SerializeField] float _aimDistance = 20f;
+    [SerializeField] float _arrowLaunchVelocity = 2000f;
+
     [Header("UI")]
     [SerializeField] GameObject _reticle;
 
@@ -48,9 +54,18 @@ public class PlayerWeaponInput : MonoBehaviour {
     }
 
     void FireInput() {
-        if (_fireInput.triggered) {
-            Debug.Log("BOUM!"); // TODO
+        if (!_fireInput.triggered) {
+            return;
         }
+
+        // Where the player is aiming.
+        Ray cameraRay = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        // Where the arrow is heading at.
+        Vector3 cameraTarget = cameraRay.origin + cameraRay.direction * _aimDistance;
+        // Direction of the arrow, from the spawn position to the target.
+        Vector3 arrowDirection = (cameraTarget - _arrowSpawn.position);
+        var arrow = Instantiate(_arrowPrefab, _arrowSpawn.position, Quaternion.LookRotation(arrowDirection));
+        arrow.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, 0, _arrowLaunchVelocity));
     }
 }
 
